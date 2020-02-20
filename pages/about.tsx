87751acf8e4da71d37151aeb3787f05472011b8e
Jacob Fowles <js.fowles@gmail.com>
2020-02-19 18:05:13 -0700

    updated npm

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,7 +4,7 @@ import { Container, Row } from '@identity/grid';
 const About = () => (
   <Container space={{ padding: 'vertical-md' }}>
     <Row>
-      <h1>About Page</h1>
+      <h1 style={{ fonSize: 144 }}>About Page</h1>
     </Row>
   </Container>
 );
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { Container, Row } from '@identity/grid';
 const Home = () => (
   <Container space={{ padding: 'vertical-md' }}>
     <Row>
-      <h1>Index Page</h1>
+      <h1 styles={{ fontSize: 144 }}>Index Page</h1>
     </Row>
   </Container>
 );
